87259f39027e5356439d20be00a404e8c8c5fc05
Dev Mąż <yakuzatanaka@gmail.com>
2020-06-10 10:10:20 +0200

    first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name",defaultValue = "user")String name){
+		return String.format("Welcome, %s",name);
+	}
+	@GetMapping("/yolo")
+	public String yolo(@RequestParam(value = "string", defaultValue = "kolo") String sthg){
+		return String.format("Yolo %s",sthg);
+	}
+}
