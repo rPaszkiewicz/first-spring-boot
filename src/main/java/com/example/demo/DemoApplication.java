355106e869b66d954415c12bec90809ad7e632b5
Dev Mąż <yakuzatanaka@gmail.com>
2020-06-10 10:25:48 +0200

    created /howdy mapping

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -18,5 +18,13 @@ public class DemoApplication {
 	public String hello(@RequestParam(value = "name",defaultValue = "user")String name){
 		return String.format("Welcome, %s",name);
 	}
+	@GetMapping("/yolo")
+	public String yolo(@RequestParam(value = "string", defaultValue = "kolo") String sthg){
+		return String.format("Yolo %s",sthg);
+	}
+	@GetMapping("/howdy")
+	public String howdy(@RequestParam(value = "word", defaultValue = "default man") String name){
+		return String.format("howdy there %s",name);
+	}
 
 }
