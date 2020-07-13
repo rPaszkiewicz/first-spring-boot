376b234e6de796744cdb5c94272e448d9d30337c
radek <yakuzatanaka@gmail.com>
2020-07-13 13:06:20 +0200

    removed bug from DemoApplication classs

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,23 +11,4 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	/*@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name",defaultValue = "user")String name){
-		return String.format("Welcome, %s",name);
-	}
-	@GetMapping("/yolo")
-	public String yolo(@RequestParam(value = "string", defaultValue = "kolo") String sthg){
-		return String.format("Yolo %s",sthg);
-	}
-	@GetMapping("/howdy")
-	public String howdy(@RequestParam(value = "word", defaultValue = "default man") String name){
-		return String.format("howdy there %s",name);
-	}
-	@GetMapping("/hello/{name}")
-	public String pathVar(@PathVariable String name){
-		return String.format("hi there %s",name);
-	}*/
-
-	//return new Laptop(make,price);
-
 }
